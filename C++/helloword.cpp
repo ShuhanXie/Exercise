c0188d89b21d16be6bd629906e8abbb85bcffc99
ShuhanXie <x9789867@gmail.com>
2016-07-15 17:14:03 +0800

    7-15練習

---
+++ C++/helloword.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+using namespace std; 
+
+int main() {
+ int str; 
+  // cin>>str;
+ for(int i=0;i<5;i++){
+ cout << "Hello! World!\n"; 
+ cout << "±z¦n! C++!"<<endl;
+ cout << "test "<<i+1<<endl;
+ }
+ return 0; 
+}
---
+++ python/prog01.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2016-07-15 17:09:59
+# @Author  : Shu-Han Xie (x9789867@gmail.com)
+# @Link    : https://github.com/ShuhanXie/Exercise
+# @Version : $Id$
+
+import os
