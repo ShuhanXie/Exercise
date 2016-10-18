98ede80945ebb126a37c4fd8b663217ae5d697c2
ShuhanXie <x9789867@gmail.com>
2016-10-18 23:27:12 +0800

    re
    re

--- python/prog02.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Date    : 2016-07-15 16:52:03
-# @Author  : Your Name (you@example.org)
-# @Link    : http://example.org
-# @Version : $Id$
-
-import os
--- python/prog03.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Date    : 2016-07-15 16:52:41
-# @Author  : Your Name (you@example.org)
-# @Link    : http://example.org
-# @Version : $Id$
-
-import os
-print "prog03\n"
