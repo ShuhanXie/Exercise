fabad3ddb789fc9f1ac3c1de1567df88d79668b3
ShuhanXie <x9789867@gmail.com>
2016-12-06 13:37:45 +0800

    aa

--- python/exam3.py
+++ python/exam3.py
@@ -14,25 +14,18 @@ def killdie(num,k):
         z=0
         x=0
         for i in range(s,num,k):
-            print "i=",i
             arrz[i]=0
             x+=1
             if num-(i+1)>0 and i+k+1>num  :
-                    print "i+k=",i+k
                     z=num-(i+1)
-                    print "z=",z
                     g=k-z
                     s=g-1
-                    print "s=",s  
             else :
                 s=k-1
-        print "----------------↓"
         disArray()
-        print "x=",x
         for i in range(x):
             arrz.remove(0)
         num-=x
-        print "num- =",num        
 num=input("please enter num:")
 k=input("please enter k:")
 mkArray(num)
